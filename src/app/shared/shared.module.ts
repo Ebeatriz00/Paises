1c3397ce0c2ed486165cb95c03a79474ad3d3004
Ebeatriz00 <bfasanandodelrio@egmail.com>
2024-04-27 09:24:54 -0500

    CountryApp

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import {  RouterModule } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component'
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContactComponent,
+    SearchBoxComponent,
+    LoadingSpinnerComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContactComponent,
+    SearchBoxComponent,
+    LoadingSpinnerComponent,
+  ]
+})
+export class SharedModule { }
